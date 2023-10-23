c98573953175f9f3812068f30784a3d4d1e0d246
henriquerocharafael <rafaelrocha2500@gmail.com>
2023-10-23 18:46:10 -0300

    album select by id params

--- src/components/Projects/Albums/index.tsx
+++ src/components/Projects/Albums/index.tsx
@@ -1,23 +1,17 @@
-import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 import { useEffect, useState } from "react";
-import {
-  getDownloadURL,
-  listAll,
-  ref,
-  getStorage,
-  getMetadata,
-} from "firebase/storage";
+import { getDownloadURL, listAll, ref } from "firebase/storage";
 import { storage } from "../../../firebase";
+import { useParams } from "react-router-dom";
 
 export function Album() {
-  // const { id } = useParams();
+  const { id } = useParams();
 
   // this will return just the album with the same id as params id
   // album.id === Number(id) && (return <img />)
 
   const [imageList, setImageList] = useState<string[]>([]);
-  const imageListRef = ref(storage, "images/");
+  const imageListRef = ref(storage, `images/${id}`);
 
   useEffect(() => {
     listAll(imageListRef).then((response) => {
@@ -29,35 +23,15 @@ export function Album() {
     });
   }, []);
 
-  // Create a reference to the file whose metadata we want to retrieve
-  const storageMetaData = getStorage();
-  const hanburyRef = ref(storageMetaData, "images/Hanbury-South.jpg");
-
-  const [imgName, setImgName] = useState<string[]>([]);
-
-  // Get metadata properties
-  getMetadata(hanburyRef)
-    .then((metadata) => {
-      // Metadata now contains the metadata for 'images/Hanbury-South.jpg'
-      setImgName([...imgName, metadata.name]);
-    })
-    .catch((error) => {
-      // Uh-oh, an error occurred!
-      console.log(error);
-    });
-
   return (
     <div className={styles.container}>
+      <h1>Album {id}</h1>
+
       {imageList.map((url, idx) => (
         <div key={idx} className={styles.img_card}>
           <img src={url} />
-          <span>{imgName}</span>
         </div>
       ))}
-
-      <h1>
-        <Link to="/">Back to HomePage</Link>
-      </h1>
     </div>
   );
 }
